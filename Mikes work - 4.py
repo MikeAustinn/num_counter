6a9c5d69b364ea7b6d1cc2fd99c3f2ee5d89a253
MikeAustinn <103225984+MikeAustinn@users.noreply.github.com>
2022-04-19 14:46:39 -0700

    Add files via upload

---
+++ Mikes work - 4.py
@@ -0,0 +1,30 @@
+"""
+Beginner Problem: Write a function that searches all words in a text file and returns the sum of all integer values.
+
+File Name: sum_of_integers.py
+Name:      ?
+Course:    CPTR 141
+"""
+
+file_name = "numbers.txt"
+
+print(
+    "The sum of numbers in {} is {}".format(
+        file_name, sum_of_integers.sum_of_integers(file_name)
+    )
+)
+
+def sum_of_integers(file_name):
+    with open(file_name, "r") as file:
+        rows = file.read().splitlines()
+
+    total_sum = 0
+    for r in rows:
+        data = r.split(" ")
+        for d in data:
+            try:
+                num = int(d)
+                total_sum += num 
+            except Exception as e:
+                continue
+    return total_sum
